c11ff51ddb7393a8f33810c93b79b5f8d56728cd
UnrefinedBrain <github@unrefinedbrain.dev>
2024-02-16 17:38:28 +0000

    update template

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { createVueMetamorphCli } from 'vue-metamorph';
 import process from 'process';
 
@@ -13,5 +12,6 @@ const cli = createVueMetamorphCli({
 
 process.on('SIGQUIT', cli.abort);
 process.on('SIGTERM', cli.abort);
+process.on('SIGINT', cli.abort);
 
 cli.run();
